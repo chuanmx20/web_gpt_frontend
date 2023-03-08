26b20aed537dae42b98b18658d690bb81fdd5296
chuanmingxi <1009898302@qq.com>
2023-03-08 11:48:10 +0800

    add .env

--- src/login.js
+++ src/login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Popup from 'reactjs-popup';
 import './login.css'
-import './github-mark.png'
+import { get_json } from "./functions";
 
 const authorize_uri = 'https://github.com/login/oauth/authorize';
 const client_id = '325f0a66ca7279f6f2d1'
@@ -29,11 +29,27 @@ export class LoginControl extends React.Component {
   click_login() {
     window.location.href = `${authorize_uri}?client_id=${client_id}`;
   }
-  oauth_login(token_callback) {
+  oauth_login(set_token) {
     const params = new URLSearchParams(window.location.search);
-    const code = params.get('code')
-    let token = ''
-    token_callback(token)
+    const code = params.get('code');
+    const body = new URLSearchParams();
+    Object.entries({
+      code
+    }).forEach((param) => body.append(...param));
+    fetch(process.env.REACT_APP_API_ROOT + '/login', {
+      methos: 'POST',
+      body,
+    }).then(get_json)
+      .then((json) => {
+        if (json.code != 200) {
+          throw new Error(JSON.stringify(json));
+        }
+
+        set_token(json.token);
+        alert('Successfully logged in!');
+      });
+
+    let token = 'token'
     return true;
   }
   remove_url_params() {
@@ -72,11 +88,6 @@ export class LoginControl extends React.Component {
               </div>
             )}
           </Popup >
-        )}
-        {this.state.isLoggedIn && (
-          <p>
-            Logged In
-          </p>
         )}
       </div>
       
